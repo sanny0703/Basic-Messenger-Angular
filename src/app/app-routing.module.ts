43fffd21d4b3c634e0b898cd6c3c9a47079af284
SanjayKumarUppala <sanjaypro8b@gmail.com>
2022-02-26 16:10:17 +0530

    FrontEnd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {ViewUsersComponent} from './view-users/view-users.component';
+import {ViewMessagesComponent} from './view-messages/view-messages.component'
+const routes: Routes = [{ path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+    path:"signUp",
+    component:SignupComponent
+  },
+  {
+    path:"viewUsers",
+    component:ViewUsersComponent
+  },
+  {
+    path:"viewMessages",
+    component:ViewMessagesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-messenger'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-messenger');
+    
   });
 
   it('should render title', () => {
